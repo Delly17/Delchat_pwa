1dbca5b4b3bdd42c3df7cc573a17af61265ebc30
dellylehani <lehanidelly@gmail.com>
2025-10-23 15:58:30 +0200

    Fix: correction syntaxe fetch Service Worker

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,23 @@
-self.addEventListener("install", e => {
+self.addEventListener("install", (e) => {
   e.waitUntil(
-    caches.open("delchat-cache").then(cache => {
+    caches.open("delchat-cache").then((cache) => {
       return cache.addAll([
-        "./",
-        "./index.html",
-        "./style.css",
-        "./app.js",
-        "./manifest.json",
-        "./images/icon-192.png",
-        "./images/icon-512.png"
+        "/Delchat_pwa/",
+        "/Delchat_pwa/index.html",
+        "/Delchat_pwa/style.css",
+        "/Delchat_pwa/app.js",
+        "/Delchat_pwa/manifest.json",
+        "/Delchat_pwa/images/icon-192.png",
+        "/Delchat_pwa/images/icon-512.png"
       ]);
     })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
 });
